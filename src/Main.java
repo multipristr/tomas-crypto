256ef3ff637fe0dacf28831045d5ab49eb3c3834
Politické Spektrum <okularek@centrum.cz>
2025-02-24 11:49:46 +0000

    Fix Yahoo's rate limiting

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,7 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Scanner;
 import java.util.StringJoiner;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -349,6 +350,24 @@ public class Main {
         Pattern lowPattern = Pattern.compile("\"low\":\\[(?<low>.+?)]");
         Pattern highPattern = Pattern.compile("\"high\":\\[(?<high>.+?)]");
         Pattern closePattern = Pattern.compile("\"close\":\\[(?<close>.+?)]");
+        String[] USER_AGENTS = {
+                // Chrome
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36",
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36",
+                "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36",
+
+                // Firefox
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:135.0) Gecko/20100101 Firefox/135.0",
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 14.7; rv:135.0) Gecko/20100101 Firefox/135.0",
+                "Mozilla/5.0 (X11; Linux i686; rv:135.0) Gecko/20100101 Firefox/135.0",
+
+                // Safari
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 14_7_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.3 Safari/605.1.15",
+
+                // Edge
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36 Edg/131.0.2903.86"
+        };
+        AtomicInteger headerCounter = new AtomicInteger();
 
         return Arrays.stream(coinNames)
                 .parallel()
@@ -356,6 +375,7 @@ public class Main {
                     try {
                         String url = "https://query2.finance.yahoo.com/v8/finance/chart/" + ticker + "-USD?period1=1509494400&period2=9999999999&interval=1d&events=history";
                         HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+                        connection.setRequestProperty("User-Agent", USER_AGENTS[headerCounter.getAndIncrement() % USER_AGENTS.length]);
                         int status = connection.getResponseCode();
                         if (status > 299) {
                             StringBuilder response = new StringBuilder(url);
