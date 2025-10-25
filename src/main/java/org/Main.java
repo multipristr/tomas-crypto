277c3c63152aa9d553c4442beb9328e6a609c953
Name Surname <email@email.net>
2025-10-25 15:46:34 +0200

    Fix media share icon location

--- src/main/java/org/Main.java
+++ src/main/java/org/Main.java
@@ -51,8 +51,8 @@ public class Main {
                     "    <meta content=\"Tomas' crypto\" property=\"og:title\">\n" +
                     "    <meta content=\"\" property=\"og:determiner\">\n" +
                     "    <meta content=\"en_US\" property=\"og:locale\">\n" +
-                    "    <meta content=\"https://tomas-crypto.gitlab.io/coins/assets/favicon/android-chrome-512x512.png\" property=\"og:image\">\n" +
-                    "    <meta content=\"https://tomas-crypto.gitlab.io/coins/assets/favicon/android-chrome-512x512.png\" property=\"og:image:secure_url\">\n" +
+                    "    <meta content=\"https://raw.githubusercontent.com/multipristr/tomas-crypto/refs/heads/main/public/assets/android-chrome-512x512.png\" property=\"og:image\">\n" +
+                    "    <meta content=\"https://raw.githubusercontent.com/multipristr/tomas-crypto/refs/heads/main/public/assets/android-chrome-512x512.png\" property=\"og:image:secure_url\">\n" +
                     "    <meta content=\"image/png\" property=\"og:image:type\">\n" +
                     "    <meta content=\"512\" property=\"og:image:width\">\n" +
                     "    <meta content=\"512\" property=\"og:image:height\">\n" +
